3ac0c4a9e6c19c84698f20d14115fe02a0716408
Chris Burke <burke1791@gmail.com>
2019-12-25 07:54:27 -0600

    added auth forms and configured aws amplify

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  apiGateway: {
+    REGION: 'us-east-1',
+    URL: 'https://3tc74nf1ml.execute-api.us-east-1.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_G4k81hsV1',
+    APP_CLIENT_ID: '4p441c0igar2hrbcqbj7vefum7',
+    IDENTITY_POOL_ID: 'us-east-1:addc8456-6092-44de-9cd8-f288865aabfb'
+  }
+};
